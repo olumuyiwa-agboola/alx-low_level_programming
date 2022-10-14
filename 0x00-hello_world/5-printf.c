8880881ffeb78da7c91d88ad7622b75b1413412c
olumuyiwa-agboola <agboolaod@gmail.com>
2022-10-13 21:39:03 -0700

    Add solutions to mandatory tasks with README

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Prints out some text
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
---
+++ 0x00-hello_world/README.md
@@ -0,0 +1,1 @@
+This directory contains the solutions to the low level programming tasks in 0x00. C - Hello, World.
