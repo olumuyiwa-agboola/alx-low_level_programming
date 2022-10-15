67e3afd3580dcd369a57b3368af16c991c21c61e
olumuyiwa-agboola <agboolaod@gmail.com>
2022-10-15 07:14:12 -0700

    Add solutions to tasks to 2 and 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: prints the alphabet in lowercase and uppercase
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	for (c = 'A'; c <= 'Z'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
+
